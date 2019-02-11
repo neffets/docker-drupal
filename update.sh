fc09ca45dc48539f0911e49581750e9e59c627db
Steffen Schüssler <neffets@neffets.de>
2019-02-12 00:52:42 +0100

    old drupals needs old drush, set specific version for drupal 7

--- update.sh
+++ update.sh
@@ -18,6 +18,13 @@ declare -A phpVersions=(
 	[8.5]='7.2'
 	[8.6]='7.2'
 )
+defaultDrushVersion='7.4.0'
+declare -A drushVersions=(
+	[6]='6.7.0'
+	[7]='7.4.0'
+	[8.5]='8.1.18'
+	[8.6]='8.1.18'
+)
 
 curl -o release -fsSL 'https://www.drupal.org/node/3060/release' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: de-DE,eo;q=0.8,de;q=0.6,en-US;q=0.4,en;q=0.2'
 trap 'rm -f release' EXIT
@@ -30,6 +37,7 @@ for version in "${versions[@]}"; do
 		rcGrepV=
 	fi
     oldVersion=""
+    drushVersion=""
     if [ "6" == "$version" ]; then
         fullVersion="6.38"
         md5="2ece34c3bb74e8bff5708593fa83eaac"
@@ -47,6 +55,10 @@ for version in "${versions[@]}"; do
 	  fi
 	  md5="$(grep -A6 -m1 '>drupal-'"$fullVersion"'.tar.gz<' release | grep -A1 -m1 '"md5 hash"' | tail -1 | awk '{ print $1 }')"
     fi
+    # old drupal - old drush
+    if [ "7" == "$version" ]; then
+        drushVersion="7.4.0"
+    fi
 
 
 	#for variant in fpm-alpine fpm apache; do
@@ -63,6 +75,7 @@ for version in "${versions[@]}"; do
 				-e 's/%%VARIANT%%/'"$variant"'/' \
 				-e 's/%%VERSION%%/'"$fullVersion"'/' \
 				-e 's/%%MD5%%/'"$md5"'/' \
+				-e 's/%%DRUSH_VERSION%%/'"${drushVersions[$version]:-$defaultDrushVersion}"'/' \
 			"./Dockerfile$oldVersion-$dist.template" > "$version/$variant/Dockerfile"
 		)
 
